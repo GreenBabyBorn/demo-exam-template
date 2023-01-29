7b16dffbcb22da0650bebd206efc2be9acd5025e
greenbabyborn <greenbabyborn.com>
2023-01-29 15:21:43 +0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const menuBtn = document.querySelector(".header__btn");
+const menu = document.querySelector(".header__menu");
+menuBtn.addEventListener("click", () => {
+  menuBtn.classList.toggle("active");
+  menu.classList.toggle("active");
+});
